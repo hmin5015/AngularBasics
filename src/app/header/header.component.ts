8ece4c861c1e8f47146fcc06f2debe810dcf902b
jhmin <hmin5015@gmail.com>
2022-12-14 18:32:11 -0800

    modify header, notification

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -7,6 +7,7 @@ import { Component } from '@angular/core';
 })
 export class HeaderComponent {
   slogan: string = "Your one stop shop for everything."
+  siteImageSrc: string = "/assets/shopping.jpg"
   
   getSlogan() {
     return "This is a new slogan for this web application";
--- src/app/notification/notification.component.ts
+++ src/app/notification/notification.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-notification',
   template: `
-    <div class="alert alert-success">
+    <div class="alert alert-success" [hidden]="displayNotification">
       <p>This website uses cookies to provide better user experience.</p>
     </div>
   `,
@@ -13,5 +13,5 @@ import { Component } from '@angular/core';
   ]
 })
 export class NotificationComponent {
-
+  displayNotification: boolean = false;
 }
